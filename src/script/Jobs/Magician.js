45545cfc4102bb56b5f608cb3d39a1beb219fe5a
jung35 <jung@jungoh.me>
2017-12-03 06:58:24 -0600

    lets keep form in same file

--- src/script/Jobs/Magician.js
+++ src/script/Jobs/Magician.js
@@ -8,18 +8,52 @@ export default class Magician {
       img: img
     }
 
-    this.form = [
-      'level', 'int', 'luk', 'magic',
-      'skillMagic', 'maMastery',
-      'wandBonus'
-    ]
+    this.form = {
+      level: {
+        label: 'Level',
+        type: 'number'
+      },
+      int: {
+        label: 'Int',
+        type: 'number'
+      },
+      luk: {
+        label: 'Luk',
+        type: 'number'
+      },
+      magic: {
+        label: 'Total Magic',
+        type: 'number'
+      },
+      skillMagic: {
+        label: 'Skill Magic Attack',
+        type: 'number'
+      },
+      maMastery: {
+        label: 'Skill Mastery',
+        type: 'select',
+        options: [
+          {value: 1.25, name: 'Initial (25%)', selected: true},
+          {value: 1.75, name: 'Maxed Skill (25% + 50%)'}
+        ]
+      },
+      wandBonus: {
+        label: 'Elemental Wand Bonus',
+        type: 'select',
+        options: [
+          {value: 1.00, name: 'No Bonus', selected: true},
+          {value: 1.05, name: '5% Bonus'},
+          {value: 1.10, name: '10% Bonus'}
+        ]
+      }
+    }
 
     this.values = {}
   }
 
   damage() {
-    for(let i = 0; i < this.form.length; i++) {
-      if(this.values[this.form[i]] == undefined) return {}
+    for(let formField in this.form) {
+      if(this.values[formField] == undefined) return {}
     }
 
     const { level, int, luk, magic, skillMagic, maMastery, wandBonus } = this.values
