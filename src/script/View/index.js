5fe5772060a2dfb9f62a32b637993de1b9ac6f2e
jung35 <jung@jungoh.me>
2017-12-03 07:06:07 -0600

    wao query strings

--- src/script/View/index.js
+++ src/script/View/index.js
@@ -8,12 +8,37 @@ const jobList = _.map(Jobs, obj => {
   return new obj()
 })
 
+const queryString = (name) => {
+  const url = window.location.href
+  name = name.replace(/[\[\]]/g, "\\$&")
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`)
+  const results = regex.exec(url)
+  if (!results) return null
+  if (!results[2]) return ''
+  return decodeURIComponent(results[2].replace(/\+/g, " "))
+}
+
 export default class View extends Component {
   constructor(props) {
     super(props)
+    const jobQuery = queryString('job')
+
     this.state = {
       job: undefined
     }
+
+    if(jobQuery != null) {
+      for(let i = 0; i < jobList.length; i++) {
+        if(jobList[i].info.name.toLowerCase() == jobQuery.toLowerCase()) {
+          this.state = {
+            job: jobList[i]
+          }
+
+          break
+        }
+      }
+    }
+
   }
 
   redoJobSelection() {
