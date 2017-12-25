74106b99421f35dbd8fdf584ce379cddd50e922b
Jung Oh <jung@jungoh.me>
2017-12-25 16:20:37 -0600

    started somthing

---
+++ src/script/Skills/Bowman.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name: 'Basic Attack',
+    type: 'none',
+    values: [{skillDamage: 1}]
+  }, {
+    name: 'Double Shot',
+    type: 'none',
+    values: [
+      {skillDamage: 0.76*2}, {skillDamage: 0.78*2}, {skillDamage: 0.80*2}, {skillDamage: 0.82*2},
+      {skillDamage: 0.86*2}, {skillDamage: 0.88*2}, {skillDamage: 0.90*2}, {skillDamage: 0.94*2},
+      {skillDamage: 0.96*2}, {skillDamage: 1.00*2}, {skillDamage: 1.02*2}, {skillDamage: 1.06*2},
+      {skillDamage: 1.08*2}, {skillDamage: 1.12*2}, {skillDamage: 1.14*2}, {skillDamage: 1.18*2},
+      {skillDamage: 1.20*2}, {skillDamage: 1.24*2}, {skillDamage: 1.26*2}, {skillDamage: 1.30*2},
+    ]
+  }, {
+    name: 'Arrow Blow',
+    type: 'none',
+    values: [
+      {skillDamage: 1.41}, {skillDamage: 1.46}, {skillDamage: 1.51}, {skillDamage: 1.56},
+      {skillDamage: 1.64}, {skillDamage: 1.69}, {skillDamage: 1.74}, {skillDamage: 1.82},
+      {skillDamage: 1.87}, {skillDamage: 1.95}, {skillDamage: 2.00}, {skillDamage: 2.08},
+      {skillDamage: 2.13}, {skillDamage: 2.21}, {skillDamage: 2.26}, {skillDamage: 2.34},
+      {skillDamage: 2.39}, {skillDamage: 2.47}, {skillDamage: 2.52}, {skillDamage: 2.60},
+    ]
+  }, {
+    name: 'Power Knock-Back',
+    type: 'none',
+    values: [
+      {skillDamage: 1.05}, {skillDamage: 1.10}, {skillDamage: 1.15}, {skillDamage: 1.20},
+      {skillDamage: 1.25}, {skillDamage: 1.30}, {skillDamage: 1.35}, {skillDamage: 1.40},
+      {skillDamage: 1.45}, {skillDamage: 1.45}, {skillDamage: 1.55}, {skillDamage: 1.60},
+      {skillDamage: 1.65}, {skillDamage: 1.70}, {skillDamage: 1.75}, {skillDamage: 1.80},
+      {skillDamage: 1.85}, {skillDamage: 1.90}, {skillDamage: 1.95}, {skillDamage: 2.00},
+    ]
+  }
+]
